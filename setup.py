97a34fad6bb0b66da8a0313956a8a3c3a894954d
marcolongfils90 <marcolongfils@gmail.com>
2024-03-04 16:54:45 +0100

    Add requirements and setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "1.0.0"
+
+PROJECT_NAME = "MLOps"
+AUTHOR_NAME = "marcolongfils90"
+AUTHOR_EMAIL = "marcolongfils@gmail.com"
+SRC_REPO = "ml_project"
+
+REQUIRED_PACKAGES = [
+    'absl-py', 'altair', 'numpy>=1.8.0rc1', 'pandas', 'matplotlib', 'scipy',
+    'seaborn', 'six', 'statsmodels'
+]
+
+setuptools.setup(
+    name=PROJECT_NAME,
+    version=__version__,
+    description=("Simple example of an end-to-end ML pipeline."),
+    author=AUTHOR_NAME,
+    author_email=AUTHOR_EMAIL,
+    long_description=long_description,
+    long_description_content="text/markdown",
+    license="Apache 2.0",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=REQUIRED_PACKAGES,
+    python_requires='>=3.6')
